0bf94b03279dd4045a9f7a2a644a55bf51a167bb
ChandlerJoseph <142134477+ChandlerJoseph@users.noreply.github.com>
2023-08-15 20:17:33 -0500

    Update InterfaceImprovements.lua

--- InterfaceImprovements.lua
+++ InterfaceImprovements.lua
@@ -2,159 +2,136 @@
 -- Tables
 ----------------------------------------------------------------------------------------------------
 local defaults = {
-	hideEmptyButtons = false,
+    hideEmptyButtons = false,
+    hideExtraButtonArtwork = false,
     hideHotkeys = false,
     hideMacroNames = false,
-	hideStanceBar = false,
-    hideXPBar = false
+    hideStanceBar = false,
+    hideXPBar = false,
 }
 
-local buttons = {
-    ExtraActionButton1
+local bars = {
+    MainMenuBar,
+    MultiBarBottomLeft,
+    MultiBarBottomRight,
+    MultiBarLeft,
+    MultiBarRight,
+    MultiBar5,
+    MultiBar6,
+    MultiBar7,
 }
-
-for i = 1, 12 do
-    tinsert(buttons, _G["ActionButton"..i])
-    tinsert(buttons, _G["MultiBarBottomLeftButton"..i])
-    tinsert(buttons, _G["MultiBarBottomRightButton"..i])
-    tinsert(buttons, _G["MultiBarLeftButton"..i])
-    tinsert(buttons, _G["MultiBarRightButton"..i])
-    tinsert(buttons, _G["MultiBar5Button"..i])
-    tinsert(buttons, _G["MultiBar6Button"..i])
-    tinsert(buttons, _G["MultiBar7Button"..i])
-end
-
-for i = 1, 6 do
-    tinsert(buttons, _G["OverrideActionBarButton"..i])
-end
-
-local petButtons = {}
-
-for i = 1, 10 do
-    tinsert(petButtons, _G["PetActionButton"..i])
-end
 ----------------------------------------------------------------------------------------------------
 -- Functions
 ----------------------------------------------------------------------------------------------------
-ExtraActionButton1.style:SetAlpha(0)
-ZoneAbilityFrame.Style:SetAlpha(0)
-
-local function showEmptyButtons()
-	for i = 1, 12 do
-		_G["ActionButton"..i]:SetAlpha(1)
-	end
+local function showButtons()
+    for _, button in pairs(MainMenuBar.actionButtons) do
+        button:SetAlpha(1)
+    end
 end
 
-local function hideEmptyButtons()
-	for i = 1, 12 do
-		if not _G["ActionButton"..i]:HasAction() then
-			_G["ActionButton"..i]:SetAlpha(0)
-		end
-	end
+local function updateButtons()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        for _, button in pairs(MainMenuBar.actionButtons) do
+            if not button:HasAction() then
+                button:SetAlpha(0)
+            else
+                button:SetAlpha(1)
+            end
+        end
+    end
 end
 
-local function toggleEmptyButtons()
-	if InterfaceImprovementsDB.hideEmptyButtons then
-		hideEmptyButtons()
-	else
-		showEmptyButtons()
-	end
+local function updateHotkeys()
+    local alpha = InterfaceImprovementsDB.hideHotkeys and 0 or 1
+    for _, bar in pairs(bars) do
+        for _, button in pairs(bar.actionButtons) do
+            button.HotKey:SetAlpha(alpha)
+        end
+    end
+    for _, button in pairs(PetActionBar.actionButtons) do
+        button.HotKey:SetAlpha(alpha)
+    end
 end
 
-SpellBookFrame:HookScript("OnShow", function()
-	if InterfaceImprovementsDB.hideEmptyButtons then
-		showEmptyButtons()
-	end
-end)
+local function updateMacroNames()
+    local alpha = InterfaceImprovementsDB.hideMacroNames and 0 or 1
+    for _, bar in pairs(bars) do
+        for _, button in pairs(bar.actionButtons) do
+            button.Name:SetAlpha(alpha)
+        end
+    end
+end
 
-SpellBookFrame:HookScript("OnHide", function()
-	if InterfaceImprovementsDB.hideEmptyButtons then
-		if not GetCursorInfo() then
-			hideEmptyButtons()
-		end
-	end
-end)
+local function updateExtraButtonArtwork()
+    local alpha = InterfaceImprovementsDB.hideExtraButtonArtwork and 0 or 1
+    ExtraActionButton1.style:SetAlpha(alpha)
+    ZoneAbilityFrame.Style:SetAlpha(alpha)
+end
 
-QuickKeybindFrame:HookScript("OnShow", function()
-	if InterfaceImprovementsDB.hideEmptyButtons then
-		showEmptyButtons()
-	end
-end)
+local function updateStanceBar()
+    local alpha = InterfaceImprovementsDB.hideStanceBar and 0 or 1
+    StanceBar:SetAlpha(alpha)
+end
 
-QuickKeybindFrame:HookScript("OnHide", function()
-	if InterfaceImprovementsDB.hideEmptyButtons then
-		hideEmptyButtons()
-	end
-end)
+local function updateXPBar()
+    local alpha = InterfaceImprovementsDB.hideXPBar and 0 or 1
+    StatusTrackingBarManager:SetAlpha(alpha)
+end
 
 local function shortenHotkeys(self)
     local hotkey = self.HotKey
     local text = hotkey:GetText()
-
+    
     text = gsub(text, "(s%-)", "S")
     text = gsub(text, "(c%-)", "C")
     text = gsub(text, "(a%-)", "A")
     text = gsub(text, KEY_MOUSEWHEELUP, "WU")
     text = gsub(text, KEY_MOUSEWHEELDOWN, "WD")
-
+    
     for i = 1, 12 do
         text = gsub(text, _G["KEY_BUTTON"..i], "M"..i)
     end
-
+    
     hotkey:SetText(text)
 end
 
-local function hookHotkeys(table)
-    for _, v in pairs(table) do
-        hooksecurefunc(v, "UpdateHotkeys", shortenHotkeys)
-    end
-end
-hookHotkeys(buttons)
-
-local function hookPetHotkeys(table)
-    for _, v in pairs(table) do
-        hooksecurefunc(v, "SetHotkeys", shortenHotkeys)
-    end
-end
-hookPetHotkeys(petButtons)
-
-local function toggleHotkeys()
-    local alpha = InterfaceImprovementsDB.hideHotkeys and 0 or 1
-    for i = 1, 12 do
-        _G["ActionButton"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBarBottomLeftButton"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBarBottomRightButton"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBarLeftButton"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBarRightButton"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBar5Button"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBar6Button"..i.."HotKey"]:SetAlpha(alpha)
-        _G["MultiBar7Button"..i.."HotKey"]:SetAlpha(alpha)
+for _, bar in pairs(bars) do
+    for _, button in pairs(bar.actionButtons) do
+        hooksecurefunc(button, "UpdateHotkeys", shortenHotkeys)
     end
 end
 
-local function toggleMacroNames()
-    local alpha = InterfaceImprovementsDB.hideMacroNames and 0 or 1
-    for i = 1, 12 do
-        _G["ActionButton"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBarBottomLeftButton"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBarBottomRightButton"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBarLeftButton"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBarRightButton"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBar5Button"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBar6Button"..i.."Name"]:SetAlpha(alpha)
-        _G["MultiBar7Button"..i.."Name"]:SetAlpha(alpha)
+for _, button in pairs(PetActionBar.actionButtons) do
+    hooksecurefunc(button, "SetHotkeys", shortenHotkeys)
+end
+
+for i = 1, 6 do
+    hooksecurefunc(_G["OverrideActionBarButton"..i], "UpdateHotkeys", shortenHotkeys)
+end
+
+hooksecurefunc(ExtraActionButton1, "UpdateHotkeys", shortenHotkeys)
+
+SpellBookFrame:HookScript("OnShow", function()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        showButtons()
     end
-end
+end)
 
-local function toggleStanceBar()
-	local alpha = InterfaceImprovementsDB.hideStanceBar and 0 or 1
-	StanceBar:SetAlpha(alpha)
-end
+SpellBookFrame:HookScript("OnHide", function()
+    if not GetCursorInfo() then
+        updateButtons()
+    end
+end)
 
-local function toggleXPBar()
-    local alpha = InterfaceImprovementsDB.hideXPBar and 0 or 1
-    StatusTrackingBarManager:SetAlpha(alpha)
-end
+QuickKeybindFrame:HookScript("OnShow", function()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        showButtons()
+    end
+end)
+
+QuickKeybindFrame:HookScript("OnHide", function()
+    updateButtons()
+end)
 ----------------------------------------------------------------------------------------------------
 -- Config
 ----------------------------------------------------------------------------------------------------
@@ -171,15 +148,27 @@ hideEmptyButtonsCB:SetPoint("TOPLEFT", title, -3, -24)
 hideEmptyButtonsCB.Text:SetText("Hide Empty Buttons")
 hideEmptyButtonsCB:SetScript("OnClick", function()
     InterfaceImprovementsDB.hideEmptyButtons = not InterfaceImprovementsDB.hideEmptyButtons
-    toggleEmptyButtons()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        updateButtons()
+    else
+        showButtons()
+    end
+end)
+
+local hideExtraButtonArtworkCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+hideExtraButtonArtworkCB:SetPoint("CENTER", hideEmptyButtonsCB, 0, -31)
+hideExtraButtonArtworkCB.Text:SetText("Hide Extra Button Artwork")
+hideExtraButtonArtworkCB:SetScript("OnClick", function()
+    InterfaceImprovementsDB.hideExtraButtonArtwork = not InterfaceImprovementsDB.hideExtraButtonArtwork
+    updateExtraButtonArtwork()
 end)
 
 local hideHotkeysCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
-hideHotkeysCB:SetPoint("CENTER", hideEmptyButtonsCB, 0, -31)
+hideHotkeysCB:SetPoint("CENTER", hideExtraButtonArtworkCB, 0, -31)
 hideHotkeysCB.Text:SetText("Hide Hotkeys")
 hideHotkeysCB:SetScript("OnClick", function()
     InterfaceImprovementsDB.hideHotkeys = not InterfaceImprovementsDB.hideHotkeys
-    toggleHotkeys()
+    updateHotkeys()
 end)
 
 local hideMacroNamesCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
@@ -187,7 +176,7 @@ hideMacroNamesCB:SetPoint("CENTER", hideHotkeysCB, 0, -31)
 hideMacroNamesCB.Text:SetText("Hide Macro Names")
 hideMacroNamesCB:SetScript("OnClick", function()
     InterfaceImprovementsDB.hideMacroNames = not InterfaceImprovementsDB.hideMacroNames
-    toggleMacroNames()
+    updateMacroNames()
 end)
 
 local hideStanceBarCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
@@ -195,7 +184,7 @@ hideStanceBarCB:SetPoint("CENTER", hideMacroNamesCB, 0, -31)
 hideStanceBarCB.Text:SetText("Hide Stance Bar")
 hideStanceBarCB:SetScript("OnClick", function()
     InterfaceImprovementsDB.hideStanceBar = not InterfaceImprovementsDB.hideStanceBar
-    toggleStanceBar()
+    updateStanceBar()
 end)
 
 local hideXPBarCB = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
@@ -203,7 +192,7 @@ hideXPBarCB:SetPoint("CENTER", hideStanceBarCB, 0, -31)
 hideXPBarCB.Text:SetText("Hide XP Bar")
 hideXPBarCB:SetScript("OnClick", function()
     InterfaceImprovementsDB.hideXPBar = not InterfaceImprovementsDB.hideXPBar
-    toggleXPBar()
+    updateXPBar()
 end)
 ----------------------------------------------------------------------------------------------------
 -- Events
@@ -217,93 +206,95 @@ event:RegisterEvent("ACTIONBAR_HIDEGRID")
 event:RegisterEvent("ACTIONBAR_SLOT_CHANGED")
 event:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
 event:SetScript("OnEvent", function(self, event, ...)
-    if event == "ADDON_LOADED" then
-        local name = ...
-        if name == "InterfaceImprovements" then
-            InterfaceImprovementsDB = InterfaceImprovementsDB or {}
-            for k, v in pairs(defaults) do
-                if InterfaceImprovementsDB[k] == nil then
-                    InterfaceImprovementsDB[k] = v
-                end
+    self[event](self, ...)
+end)
+
+function event:ADDON_LOADED(name)
+    if name == "InterfaceImprovements" then
+        InterfaceImprovementsDB = InterfaceImprovementsDB or {}
+        for k, v in pairs(defaults) do
+            if InterfaceImprovementsDB[k] == nil then
+                InterfaceImprovementsDB[k] = v
             end
         end
-		if name == "Blizzard_ClassTalentUI" then
-			ClassTalentFrame:HookScript("OnShow", function()
-				if InterfaceImprovementsDB.hideEmptyButtons then
-					showEmptyButtons()
-				end
-			end)
-			ClassTalentFrame:HookScript("OnHide", function()
-				if InterfaceImprovementsDB.hideEmptyButtons then
-					if not GetCursorInfo() then
-						hideEmptyButtons()
-					end
-				end
-			end)
-		end
-    elseif event == "PLAYER_LOGIN" then
-		hideEmptyButtonsCB:SetChecked(InterfaceImprovementsDB.hideEmptyButtons)
-        hideHotkeysCB:SetChecked(InterfaceImprovementsDB.hideHotkeys)
-        hideMacroNamesCB:SetChecked(InterfaceImprovementsDB.hideMacroNames)
-		hideStanceBarCB:SetChecked(InterfaceImprovementsDB.hideStanceBar)
-        hideXPBarCB:SetChecked(InterfaceImprovementsDB.hideXPBar)
-    elseif event == "PLAYER_ENTERING_WORLD" then
-		toggleEmptyButtons()
-        toggleHotkeys()
-        toggleMacroNames()
-		toggleStanceBar()
-        toggleXPBar()
-	elseif event == "ACTIONBAR_SHOWGRID" then
-		if InterfaceImprovementsDB.hideEmptyButtons then
-			showEmptyButtons()
-		end
-	elseif event == "ACTIONBAR_HIDEGRID" then
-		if InterfaceImprovementsDB.hideEmptyButtons then
-			if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
-				C_Timer.After(0, function()
-					hideEmptyButtons()
-				end)
-			end
-		end
-	elseif event == "ACTIONBAR_SLOT_CHANGED" then
-		if InterfaceImprovementsDB.hideEmptyButtons then
-			if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
-				if GetCursorInfo() then
-					C_Timer.After(0.01, function()
-						showEmptyButtons()
-					end)
-				else
-					hideEmptyButtons()
-				end
-			end
-		end
-	elseif event == "UPDATE_BONUS_ACTIONBAR" then
-		if InterfaceImprovementsDB.hideEmptyButtons then
-			if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
-				for i = 1, 12 do
-					if not _G["ActionButton"..i]:HasAction() then
-						_G["ActionButton"..i]:SetAlpha(0)
-					else
-						_G["ActionButton"..i]:SetAlpha(1)
-					end
-				end
-			end
-		end
-	end
-end)
+    end
+end
+
+function event:ADDON_LOADED(name)
+    if name == "Blizzard_ClassTalentUI" then
+        ClassTalentFrame:HookScript("OnShow", function()
+            if InterfaceImprovementsDB.hideEmptyButtons then
+                showButtons()
+            end
+        end)
+        ClassTalentFrame:HookScript("OnHide", function()
+            if not GetCursorInfo() then
+                updateButtons()
+            end
+        end)
+    end
+end
+
+function event:PLAYER_LOGIN()
+    hideEmptyButtonsCB:SetChecked(InterfaceImprovementsDB.hideEmptyButtons)
+    hideExtraButtonArtworkCB:SetChecked(InterfaceImprovementsDB.hideExtraButtonArtwork)
+    hideHotkeysCB:SetChecked(InterfaceImprovementsDB.hideHotkeys)
+    hideMacroNamesCB:SetChecked(InterfaceImprovementsDB.hideMacroNames)
+    hideStanceBarCB:SetChecked(InterfaceImprovementsDB.hideStanceBar)
+    hideXPBarCB:SetChecked(InterfaceImprovementsDB.hideXPBar)
+end
+
+function event:PLAYER_ENTERING_WORLD()
+    updateButtons()
+    updateExtraButtonArtwork()
+    updateHotkeys()
+    updateMacroNames()
+    updateStanceBar()
+    updateXPBar()
+end
+
+function event:ACTIONBAR_SHOWGRID()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        showButtons()
+    end
+end
+
+function event:ACTIONBAR_HIDEGRID()
+    if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
+        updateButtons()
+    end
+end
+
+function event:ACTIONBAR_SLOT_CHANGED()
+    if InterfaceImprovementsDB.hideEmptyButtons then
+        if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
+            if GetCursorInfo() then
+                showButtons()
+            else
+                updateButtons()
+            end
+        end
+    end
+end
+
+function event:UPDATE_BONUS_ACTIONBAR()
+    if not SpellBookFrame:IsShown() and (not IsAddOnLoaded("Blizzard_ClassTalentUI") or not ClassTalentFrame:IsShown()) then
+        updateButtons()
+    end
+end
 ----------------------------------------------------------------------------------------------------
--- Slash commands
+-- Slash Commands
 ----------------------------------------------------------------------------------------------------
 SLASH_reloadUI1 = "/rl"
-SlashCmdList.reloadUI = ReloadUI
+SlashCmdList.reloadUI = reloadUI
 
 SLASH_fStack1 = "/fs"
 SlashCmdList.fStack = function()
-    LoadAddOn("Blizzard_DebugTools")
-    FrameStackTooltip_Toggle()
+LoadAddOn("Blizzard_DebugTools")
+FrameStackTooltip_Toggle()
 end
 
 SLASH_InterfaceImprovements1 = "/ii"
 SlashCmdList.InterfaceImprovements = function()
-    InterfaceOptionsFrame_OpenToCategory("InterfaceImprovements")
+InterfaceOptionsFrame_OpenToCategory("InterfaceImprovements")
 end
